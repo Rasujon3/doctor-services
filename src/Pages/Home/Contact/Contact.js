cbbaf6a496c79dbef0ff3f017876ed56224c2ffe
Rasujon3 <rasujon3@gmail.com>
2022-04-17 01:20:29 +0600

    contact section done

---
+++ src/Pages/Home/Contact/Contact.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <div id="contact" className="container">
+      <h2 className="text-center text-primary my-2">Contact with me</h2>
+      <div className="contact">
+        <div className="contact-left">
+          <img
+            src="https://rasujon3.github.io/assignment-2/images/contact.png"
+            alt=""
+          />
+        </div>
+        <div className="contact-right">
+          <form action="">
+            <input type="text" placeholder="Your Name" className="text-field" />
+            <input
+              type="email"
+              placeholder="Your Email"
+              className="text-field"
+            />
+            <textarea
+              placeholder="Your Message"
+              className="text-field"
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+            ></textarea>
+            <button className="btn btn-primary" type="submit">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -27,11 +27,12 @@ const Header = () => {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="home#services">Services</Nav.Link>
+              <Nav.Link href="home#contact">Contact</Nav.Link>
+            </Nav>
+            <Nav>
               <Nav.Link as={Link} to="/blogs">
                 Blogs
               </Nav.Link>
-            </Nav>
-            <Nav>
               <Nav.Link as={Link} to="about">
                 About Me
               </Nav.Link>
